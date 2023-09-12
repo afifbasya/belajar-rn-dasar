8561808b39289be611311eb133c191ea666272f1
Afif Uddin <afifuddin@192.168.1.5>
2023-09-12 11:13:14 +0700

    initial

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { styles } from './styles'
 
+//1. Functional Component
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Muhammad Afifuddin</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//2. Arrow Function Component
+// const App = () => {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Muhammad Afif</Text>
+//     </View>
+//   )
+// }
+// export default App
+
+//3. Class Component
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Muhammad Afifuddin</Text>
+//       </View>
+//     )
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+// di CSS 
+// background-color: #fff; 
