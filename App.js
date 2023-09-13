a8a2a7bdee0a294029ce13726e1d0568af595962
Afif Uddin <afifuddin@192.168.1.5>
2023-09-13 09:03:58 +0700

    update border

--- App.js
+++ App.js
@@ -4,19 +4,16 @@ export default function App() {
   return (
     <View style={styles.container}>
 
-      <View style={styles.wrapperKotak}>
-        {/* Kotak Merah  */}
-        <View style={styles.merah} />
-        {/* Kotak Biru  */}
-        <View style={styles.biru} />
-        {/* Kotak Hijau */}
-        <View style={styles.hijau} />
+      <View style={styles.card}>
+        <Text>Border</Text>
       </View>
 
-      {/* 1. Flex
-      2. Flex Direction 
-      3. Justify Content 
-      4. Align Items */}
+      <View style={styles.garis} />
+
+      {/* border width 
+      border color 
+      border style 
+      border radius */}
 
     </View>
   );
@@ -26,26 +23,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    marginTop: 30
+    marginTop: 30,
   },
-  merah: {
-    backgroundColor: 'red',
+  card: {
     width: 100,
-    height: 100
+    height: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 2,
   },
-  biru: {
-    backgroundColor: 'blue',
-    width: 100,
-    height: 100
-  },
-  hijau: {
-    backgroundColor: 'green',
-    width: 100,
-    height: 100
-  },
-  wrapperKotak: {
-    flexDirection: 'row',
-    justifyContent: 'center'
+  garis: {
+    borderWidth: 1,
+    marginTop: 10
   }
 });
 
