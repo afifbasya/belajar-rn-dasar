260aa6f521803e0412eb0a74b65e379cdee818e0
Afif Uddin <afifuddin@192.168.1.5>
2023-09-13 10:11:01 +0700

    Update Position

--- App.js
+++ App.js
@@ -4,19 +4,9 @@ export default function App() {
   return (
     <View style={styles.container}>
 
-
-      {/* 1. Override Style. */}
-      <View>
-        <View style={[styles.card, { backgroundColor: 'red' }]} />
-        <View style={[styles.card, { backgroundColor: 'blue' }]} />
-        <View style={[styles.card, { backgroundColor: 'green' }]} />
-      </View>
-
-      {/* 2. Use variable. */}
-      <View>
-        <View style={styles.card2("red")} />
-        <View style={styles.card2("blue")} />
-        <View style={styles.card2("green")} />
+      <View style={styles.header}>
+        <Text style={{ color: 'white' }}>Header</Text>
+        <View style={styles.card} />
       </View>
 
     </View>
@@ -29,14 +19,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     marginTop: 30,
   },
+  header: {
+    backgroundColor: 'red',
+    height: 250,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
   card: {
     height: 100,
     width: 100,
-  },
-  card2: (color) => ({
-    height: 100,
-    width: 100,
-    backgroundColor: color,
-  })
+    backgroundColor: 'blue',
+    position: 'absolute',
+    // right: 0,
+    // top: 0,
+    // left: 0,
+    bottom: 0,
+  }
 });
 
