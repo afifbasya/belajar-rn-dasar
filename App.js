8e61743e126cc6ed26715655aeb19fd36f7a334e
Afif Uddin <afifuddin@192.168.1.5>
2023-09-13 10:23:04 +0700

    add image

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
 
-      <View style={styles.header}>
-        <Text style={{ color: 'white' }}>Header</Text>
-        <View style={styles.card} />
-      </View>
+      {/* Menggunakan URL */}
+      {/* <Image source={{ uri: "https://www.presidenri.go.id/assets/uploads/2020/02/presidenri.go.id-05022020111245-5e3a40bd7cdcb1.35250820-384x512.jpg" }} style={{ height: 250, width: 250 }} /> */}
+
+
+
+      {/* Simpan gambar di assets kemudian dipanggil */}
+      {/* <Image source={require("./assets/jokowi.jpg")} style={{ height: 250, width: 250 }} /> */}
+
+      {/* Resize Mode  */}
+      {/* 1. Contain
+      2. Center 
+      3. Strecth
+      4. Repeat 
+      5. Cover */}
+      <Image
+        source={require("./assets/jokowi.jpg")}
+        style={{ height: 250, width: 250, backgroundColor: 'green' }}
+        resizeMode='cover'
+      />
+
 
     </View>
   );
@@ -19,21 +35,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     marginTop: 30,
   },
-  header: {
-    backgroundColor: 'red',
-    height: 250,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  card: {
-    height: 100,
-    width: 100,
-    backgroundColor: 'blue',
-    position: 'absolute',
-    // right: 0,
-    // top: 0,
-    // left: 0,
-    bottom: 0,
-  }
 });
 
