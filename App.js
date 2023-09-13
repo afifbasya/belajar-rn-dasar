be54b4f34d152f1ef3bdca62ac268feb65994a6c
Afif Uddin <afifuddin@192.168.1.5>
2023-09-13 14:43:57 +0700

    [ADD] Button

--- App.js
+++ App.js
@@ -1,28 +1,20 @@
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+
+
+  const handleSubmit = () => {
+    console.log("Halooo");
+  }
+
   return (
     <View style={styles.container}>
 
-      {/* Menggunakan URL */}
-      {/* <Image source={{ uri: "https://www.presidenri.go.id/assets/uploads/2020/02/presidenri.go.id-05022020111245-5e3a40bd7cdcb1.35250820-384x512.jpg" }} style={{ height: 250, width: 250 }} /> */}
+      {/* <Button title="Submit" color="red" onPress={() => handleSubmit()} /> */}
 
-
-
-      {/* Simpan gambar di assets kemudian dipanggil */}
-      {/* <Image source={require("./assets/jokowi.jpg")} style={{ height: 250, width: 250 }} /> */}
-
-      {/* Resize Mode  */}
-      {/* 1. Contain
-      2. Center 
-      3. Strecth
-      4. Repeat 
-      5. Cover */}
-      <Image
-        source={require("./assets/jokowi.jpg")}
-        style={{ height: 250, width: 250, backgroundColor: 'green' }}
-        resizeMode='cover'
-      />
+      <TouchableOpacity style={styles.button} onPress={() => handleSubmit()} >
+        <Text style={{ color: 'white' }}>SUBMIT</Text>
+      </TouchableOpacity>
 
 
     </View>
@@ -35,5 +27,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     marginTop: 30,
   },
+  button: {
+    backgroundColor: 'red',
+    padding: 10,
+    width: 100,
+    alignItems: 'center',
+    borderRadius: 10,
+    margin: 20
+  }
 });
 
