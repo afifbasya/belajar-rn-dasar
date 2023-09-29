aa0aa67bc82a470b0fa17d04dea693e5b3bf41ab
Afif Uddin <afifuddin@192.168.1.2>
2023-09-29 10:09:13 +0700

    [UPDATE] Flatlist

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { useState } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, FlatList, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [refresh, setRefresh] = useState(false);
   const [makanans, setMakanan] = useState([
     {
       id: 1,
@@ -20,22 +21,59 @@ export default function App() {
     }
   ])
 
-  const jumlahHarga = makanans.reduce((total, item) => total + item.harga, 0)
+  const renderItem = ({ item }) => {
+    return (
+      <View key={item.id} style={styles.makanan}>
+        <Text>{item.nama}</Text>
+        <Text>Rp. {item.harga}</Text>
+      </View>
+    )
+  }
+
+  const HeaderMakanan = () => {
+    return (
+      <View style={{ marginHorizontal: 30, marginVertical: 10 }}>
+        <Text>List Makanan</Text>
+      </View>
+    )
+  }
+
+  const FooterMakanan = () => {
+    return (
+      <TouchableOpacity style={{ marginHorizontal: 30, marginVertical: 10 }}>
+        <Text>Load More</Text>
+      </TouchableOpacity>
+    )
+  }
+
+  const DataKosong = () => {
+    return (
+      <View style={styles.makanan}>
+        <Text>Data Kosong</Text>
+      </View>
+    )
+  }
+
+  const Separator = () => {
+    return (
+      <View style={{ marginTop: 30 }} />
+    )
+  }
+
+  const handleRefresh = () => {
+    setRefresh(true)
+
+    //Simulasi Ambil API
+    setTimeout(() => {
+      setRefresh(false)
+    }, 2000)
+
+  }
 
   return (
     <View style={styles.container}>
       {/* 1. Mapp */}
-      {makanans.map((makanan, index) => {
-        return (
-          <View key={index} style={styles.makanan}>
-            <Text>{makanan.nama}</Text>
-            <Text>Rp. {makanan.harga}</Text>
-          </View>
-        )
-      })}
-
-      {/* 2. Filter  */}
-      {/* {makanans.filter((makananFilter) => makananFilter.harga > 11000).map((makanan, index) => {
+      {/* {makanans.map((makanan, index) => {
         return (
           <View key={index} style={styles.makanan}>
             <Text>{makanan.nama}</Text>
@@ -44,10 +82,20 @@ export default function App() {
         )
       })} */}
 
-      {/* 3. Reduce  */}
-      <View style={{ margin: 30 }}>
-        <Text>Jumlah Harga : Rp. {jumlahHarga}</Text>
-      </View>
+      {/* <View style={{ height: 200 }}> */}
+      <FlatList
+        data={makanans}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        ListHeaderComponent={HeaderMakanan}
+        ListFooterComponent={FooterMakanan}
+        ListEmptyComponent={DataKosong}
+        ItemSeparatorComponent={Separator}
+        onRefresh={() => handleRefresh()}
+        refreshing={refresh}
+      // showsVerticalScrollIndicator={false}
+      />
+      {/* </View> */}
 
     </View>
   );
@@ -65,7 +113,6 @@ const styles = StyleSheet.create({
   },
   makanan: {
     marginHorizontal: 30,
-    marginTop: 30,
     borderWidth: 1,
     padding: 10
   }
