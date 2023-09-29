d8d1f5ff33d21eb94a0d91907c0179d8f8c14c17
Afif Uddin <afifuddin@192.168.1.2>
2023-09-29 20:35:20 +0700

    [UPDATE] Select Option

--- App.js
+++ App.js
@@ -1,19 +1,58 @@
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
+import { Dropdown } from 'react-native-element-dropdown';
+import axios from 'axios';
+
+const pendidikans = [
+  {
+    id: 1,
+    label: "SD"
+  },
+  {
+    id: 2,
+    label: "SMP"
+  },
+  {
+    id: 3,
+    label: "SMA"
+  },
+  {
+    id: 4,
+    label: "S1"
+  },
+  {
+    id: 5,
+    label: "S2"
+  },
+]
 
 export default function App() {
-  const [username, setUsername] = useState("");
-  const [nohp, setNoHP] = useState("");
-  const [alamat, setAlamat] = useState("");
-  const [password, setPassword] = useState("");
-  const [hidden, setHidden] = useState(true);
+  const [pendTerakhir, setPendidikan] = useState(false);
+  const [guru, setGuru] = useState(false);
+  const [dataGuru, setDataGuru] = useState([]);
+
+  useEffect(() => {
+
+    const getGuru = async () => {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+
+        setDataGuru(response.data)
+
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    getGuru();
+
+  }, [])
+
 
   const handleSubmit = () => {
     const params = {
-      username,
-      nohp,
-      alamat,
-      password
+      pendTerakhir,
+      guru
     }
 
     console.log("Params : ", params);
@@ -21,56 +60,45 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <View style={styles.wrapperInput}>
-        <Text>Username : </Text>
-        <TextInput
-          value={username}
-          style={styles.input}
-          placeholder='Masukkan Username'
-          onChangeText={(value) => setUsername(value)}
+
+      <View style={styles.wrapper}>
+        <Text>Pendidikan Terakhir : </Text>
+        <Dropdown
+          style={styles.dropdown}
+          data={pendidikans}
+          search
+          labelField="label"
+          valueField="label"
+          placeholder='Pilih Pendidikan'
+          searchPlaceholder="Cari"
+          onChange={item => {
+            setPendidikan(item.label)
+          }}
+          value={pendTerakhir}
         />
       </View>
 
-      <View style={styles.wrapperInput}>
-        <Text>No. HP : </Text>
-        <TextInput
-          value={nohp}
-          style={styles.input}
-          placeholder='Masukkan No. HP'
-          onChangeText={(value) => setNoHP(value)}
-          inputMode='tel'
+      <View style={styles.wrapper}>
+        <Text>Guru : </Text>
+        <Dropdown
+          style={styles.dropdown}
+          data={dataGuru}
+          search
+          labelField="name"
+          valueField="name"
+          placeholder='Pilih Guru'
+          searchPlaceholder="Cari"
+          onChange={item => {
+            setGuru(item.name)
+          }}
+          value={guru}
         />
-      </View>
-
-      <View style={styles.wrapperInput}>
-        <Text>Alamat : </Text>
-        <TextInput
-          value={alamat}
-          style={styles.inputMulti}
-          placeholder='Masukkan Alamat'
-          onChangeText={(value) => setAlamat(value)}
-          multiline
-          numberOfLines={4}
-        />
-      </View>
-
-      <View style={styles.wrapperInput}>
-        <Text>Password : </Text>
-        <TextInput
-          value={password}
-          style={styles.input}
-          placeholder='Masukkan Password'
-          onChangeText={(value) => setPassword(value)}
-          secureTextEntry={hidden}
-        />
-        <TouchableOpacity style={styles.show} onPress={() => setHidden(!hidden)}>
-          <Text>{hidden ? "Show" : "Hide"}</Text>
-        </TouchableOpacity>
       </View>
 
       <TouchableOpacity onPress={() => handleSubmit()} style={styles.button}>
         <Text style={{ color: 'white' }}>Submit</Text>
       </TouchableOpacity>
+
     </View>
   )
 }
@@ -81,38 +109,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFF',
     flex: 1
   },
-  wrapperInput: {
-    marginHorizontal: 30,
-    marginTop: 10,
-  },
-  input: {
-    borderWidth: 1,
-    fontSize: 15,
-    paddingHorizontal: 10,
-    paddingVertical: 8,
-    marginTop: 5,
-    borderRadius: 5
-  },
-  inputMulti: {
-    borderWidth: 1,
-    fontSize: 15,
-    paddingHorizontal: 10,
-    paddingVertical: 8,
-    marginTop: 5,
-    borderRadius: 5,
-    textAlignVertical: 'top'
-  },
-  show: {
-    position: "absolute",
-    right: 10,
-    top: 35
-  },
   button: {
     marginHorizontal: 30,
     backgroundColor: 'blue',
     marginTop: 20,
     paddingVertical: 15,
     alignItems: 'center',
+    borderRadius: 5
+  },
+  wrapper: {
+    marginTop: 10,
+    marginHorizontal: 30
+  },
+  dropdown: {
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginTop: 5,
     borderRadius: 5
   }
 })
